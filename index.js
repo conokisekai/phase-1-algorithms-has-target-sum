5c288312e163b586b86d40af9f30ee8819c19d2f
Conrad Kambi <kambiconrad@gmail.com>
2023-10-08 09:59:11 +0300

    Cleaning up the code

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
-// Pseudocode:
-// Initialize an empty set called seenNumbers.
-// Iterate through each number in the input array.
-//   Calculate the complement by subtracting the current number from the target.
-//   Check if the complement exists in the seenNumbers set.
-//   If it does, return true (a pair is found).
-//   Otherwise, add the current number to the seenNumbers set.
-// If the loop completes without finding a pair, return false.
-
 function hasTargetSum(array, target) {
-  // Initialize an empty set called seenNumbers.
   const seenNumbers = new Set();
-
-  // Iterate through each number in the input array.
   for (const number of array) {
-    // Calculate the complement by subtracting the current number from the target.
     const complement = target - number;
-
-    // Check if the complement exists in the seenNumbers set.
     if (seenNumbers.has(complement)) {
-      return true; // a pair is found
+      return true;
     }
-
-    // Otherwise, add the current number to the seenNumbers set.
     seenNumbers.add(number);
   }
-
-  // If the loop completes without finding a pair, return false.
   return false;
 }
-
-// Export the hasTargetSum function to make it accessible from other modules.
 module.exports = hasTargetSum;
 
 console.log("Expecting: true");
